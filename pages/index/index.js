5993a250fa275c86e4b67444e122b3b23b835b01
pcrab <pxw0717@gmail.com>
2020-01-06 22:26:00 +0800

    init repo

---
+++ pages/index/index.js
@@ -0,0 +1,17 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    index_image: "/images/index-img.png",
+  },
+  //事件处理函数
+  onLoad: function () {
+
+  },
+
+  getUserInfo: function (e) {
+
+  }
+})
